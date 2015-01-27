0fec0fa3c7d81d8268068a66c502a7087c2f6cf5
Marco <makrokorg@gmail.com>
2015-01-26 23:00:01 -0800

    added Path.cs

---
+++ Scripts/Paths.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paths : MonoBehaviour {
+	public float moveSpeed;
+	public GameObject[] pathStart;
+	public GameObject[] pathEnd;
+	public GameObject[] pedestrians;
+	GameObject Player;
+	
+	
+	void Start() {
+		Player = GameObject.FindGameObjectWithTag ("Player");
+		Debug.Log("Hello");
+	}
+	
+	void Update() {
+		
+		for (int i = 0; i < pedestrians.Length; i++) {
+			
+			pedestrians[i].gameObject.transform.position = Vector2.Lerp (pedestrians[i].gameObject.transform.localPosition, pathEnd[i].gameObject.transform.position, Time.deltaTime * moveSpeed);
+			
+		}
+		
+		
+		
+	}
+}
+
