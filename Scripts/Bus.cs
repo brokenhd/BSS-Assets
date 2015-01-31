be124d6757c0c2807c2c605b89354a97a5a11288
Marco <makrokorg@gmail.com>
2015-01-31 02:01:13 -0800

    bus-cs was out of the scirpts. adding it now.

---
+++ Scripts/Bus.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bus : MonoBehaviour {
+	public static bool bus;
+	public bool lift;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.gameObject.name == "Main Camera")
+		bus = true;
+	}
+
+
+	void OnTriggerExit2D(Collider2D other) {
+		if(other.gameObject.name == "Main Camera")
+		bus = false;
+	}
+}
